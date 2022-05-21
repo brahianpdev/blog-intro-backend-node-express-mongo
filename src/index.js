55130af050d4f61fbf28e7073783e89bd06575e7
brahianpdev <zk.spywn@gmail.com>
2022-05-21 16:42:41 -0300

    feat: entry point

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const config = require('./config');
+const indexRoutes = require('./routes');
+
+mongoose
+  .connect(config.db.mongo)
+  .then((res) => {
+
+    const app = express();
+    
+    app.use(
+      express.urlencoded({
+        extended: true,
+      })
+    );
+
+    app.use(express.json());
+
+    app.use(cors());
+
+    app.use("/", indexRoutes);
+
+    app.listen(config.app.port, () => {
+      console.log(`🔥 Server is running at port ${config.app.port}`);
+    });
+
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => console.log(error));
